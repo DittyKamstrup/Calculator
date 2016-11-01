bf70700fa8beb1d314823b0a8400b45984334de8
Ditte <Ditte@DITTES-PC>
2016-11-01 10:38:51 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tast tal");
+
+            string input1 = Console.ReadLine();
+
+            int tal1 = Int32.Parse(input1);
+
+            Console.WriteLine("Tast +, -, * eller /");
+
+            string input2 = Console.ReadLine();
+
+            int faktora = Int32.Parse(input2);
+
+            Console.WriteLine("Tast tal");
+
+            string input3 = Console.ReadLine();
+
+            int tal2 = Int32.Parse(input3);
+
+
+        }
+    }
+}
